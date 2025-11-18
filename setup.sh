fd7793773d870d44950e48f157a41439e148aa16
noeltz <noeltz@web.de>
2025-11-18 10:31:22 +0100

    fix

--- setup.sh
+++ setup.sh
@@ -157,21 +157,18 @@ for package in */; do
             if [ -e "$full_path" ]; then
                 mv "$full_path" "$BACKUP_DIR/$conflict_path"
                 echo "Moved existing '$full_path' to '$BACKUP_DIR/$conflict_path'"
+                stow -R -t "$TARGET_DIR" $package --no-folding
             else
                 echo "Warning: Conflict reported for '$full_path', but it doesn't exist. Skipping backup."
             fi
         done
     else
         echo "No conflicts found for $package during dry run."
+        stow -R -t "$TARGET_DIR" $package --no-folding
     fi
 done
 
-echo "Backup phase complete. Proceeding with stow."
-
-# Run stow for all packages
-stow -t "$TARGET_DIR" . --no-folding
-
-echo "Stow process finished successfully."
+echo "Backup and stow process finished successfully."
 
 echo "==> Creating systemd user services..."
 systemctl --user add-wants niri.service hypridle.service
