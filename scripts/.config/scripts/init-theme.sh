2fb8edf90f4e2c7f50cad7473c03d5e621c18805
noeltz <noeltz@web.de>
2025-11-20 13:18:04 +0100

    fix

--- scripts/.config/scripts/init-theme.sh
+++ scripts/.config/scripts/init-theme.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 # --- Init default theme ---
 if ! [ -f $HOME/.cache/theme-sync-state ]; then
     swww img $HOME/.local/share/wallpaper/Material/Dark/default.jpg
-    sleep 0.8
+    sleep 1
     bash $HOME/.config/scripts/theme-sync.sh
 fi
